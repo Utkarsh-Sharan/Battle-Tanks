79417f26b10d76bbdb4e52c6abaac9e358a4590d
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-11-18 10:44:57 +0530

    spawned tank with MVC

--- Assets/Scripts/TankMVC/TankModel.cs
+++ Assets/Scripts/TankMVC/TankModel.cs
@@ -2,5 +2,15 @@
 
 public class TankModel
 {
-    
+    private TankController _tankController;
+
+    public TankModel() 
+    {
+        
+    }
+
+    public void SetTankController(TankController tankController)
+    {
+        _tankController = tankController;
+    }
 }
--- Assets/Scripts/TankMVC/TankView.cs
+++ Assets/Scripts/TankMVC/TankView.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TankView : MonoBehaviour
 {
+    private TankController _tankController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,10 @@ public class TankView : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetTankController(TankController tankController)
+    {
+        _tankController = tankController;
     }
 }
