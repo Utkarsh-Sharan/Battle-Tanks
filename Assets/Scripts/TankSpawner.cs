8f50200fc3f670019459228c05c695022ed1e95c
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-11-18 10:14:33 +0530

    created tank spawner

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _tankPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(_tankPrefab, transform.position, Quaternion.identity);
+    }
+}
