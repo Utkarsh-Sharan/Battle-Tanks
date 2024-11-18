a1ae52499fcca73e59a97c78b1a14007dcf6d09a
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-11-18 11:18:48 +0530

    tank movement and rotation

--- Assets/Scripts/TankMVC/TankController.cs
+++ Assets/Scripts/TankMVC/TankController.cs
@@ -5,14 +5,27 @@ public class TankController
     private TankModel _tankModel;
     private TankView _tankView;
 
+    private Rigidbody _tankBody;
+
     public TankController(TankModel tankModel, TankView tankView)
     {
         _tankModel = tankModel;
-        _tankView = tankView;
+        _tankView = GameObject.Instantiate<TankView>(tankView);
+        _tankBody = _tankView.GetRigidBody();
 
         _tankModel.SetTankController(this);
         _tankView.SetTankController(this);
+    }
 
-        GameObject.Instantiate(_tankView.gameObject, _tankView.transform.position, Quaternion.identity);
+    public void Move(float movement, float movementSpeed)
+    {
+        _tankBody.velocity = _tankView.transform.forward * movement * movementSpeed;
+    }
+
+    public void Rotate(float rotate, float rotationSpeed)
+    {
+        Vector3 vector = new Vector3(0f, rotate * rotationSpeed, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(vector * Time.deltaTime);
+        _tankBody.MoveRotation(_tankBody.rotation * deltaRotation);
     }
 }
--- Assets/Scripts/TankMVC/TankView.cs
+++ Assets/Scripts/TankMVC/TankView.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class TankView : MonoBehaviour
 {
+    [SerializeField] private Rigidbody _tankBody;
+
     private TankController _tankController;
+
+    private float _movement;
+    private float _rotation;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +20,32 @@ public class TankView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+
+        if(_movement != 0)
+        {
+            _tankController.Move(_movement, 30);    //will give speed with model later.
+        }
+
+        if(_rotation != 0)
+        {
+            _tankController.Rotate(_rotation, 20);  //will give rotation with model later.
+        }
+    }
+
+    private void Movement()
+    {
+        _movement = Input.GetAxis("Vertical");
+        _rotation = Input.GetAxis("Horizontal");
     }
 
     public void SetTankController(TankController tankController)
     {
         _tankController = tankController;
+    }
+
+    public Rigidbody GetRigidBody()
+    {
+        return _tankBody;
     }
 }
